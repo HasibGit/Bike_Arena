fc61fcbf5d976ddb3b6f1f5626d6b119b0825c71
HasibGit <48997902+HasibGit@users.noreply.github.com>
2022-03-12 15:49:30 +0600

    Showing records in material table with pagination and filtering functionalities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+
 
 
 
@@ -42,7 +47,10 @@ import { HttpClientModule } from '@angular/common/http';
     ReactiveFormsModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    HttpClientModule
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [MatDatepickerModule],
   bootstrap: [AppComponent]
